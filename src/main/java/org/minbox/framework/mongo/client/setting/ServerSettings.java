ce6ca7e75d21e633af2f88cbb8673750be45dd59
hengboy <jnyuqy@gmail.com>
2020-07-07 12:01:29 +0800

    add server settings bean

---
+++ src/main/java/org/minbox/framework/mongo/client/setting/ServerSettings.java
@@ -0,0 +1,16 @@
+package org.minbox.framework.mongo.client.setting;
+
+import lombok.Data;
+
+/**
+ * Settings relating to monitoring of each server.
+ *
+ * @author 恒宇少年
+ * @see com.mongodb.connection.ServerSettings
+ * @since 1.0
+ */
+@Data
+public class ServerSettings {
+    private long heartbeatFrequencyMilliSeconds;
+    private long minHeartbeatFrequencyMilliSeconds;
+}
