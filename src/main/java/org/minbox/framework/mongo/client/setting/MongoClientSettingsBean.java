46d6e2d508eab2c2cfb60661b13f2a1468b0363b
hengboy <jnyuqy@gmail.com>
2020-07-07 12:02:50 +0800

    add more settings fields

--- src/main/java/org/minbox/framework/mongo/client/setting/MongoClientSettingsBean.java
+++ src/main/java/org/minbox/framework/mongo/client/setting/MongoClientSettingsBean.java
@@ -7,17 +7,45 @@ import org.springframework.boot.context.properties.NestedConfigurationProperty;
  * The {@link com.mongodb.MongoClientSettings} encapsulated bean
  *
  * @author 恒宇少年
+ * @since 1.0
  */
 @Data
 public class MongoClientSettingsBean {
+    private String applicationName;
+
     /**
      * The socket settings
      */
     @NestedConfigurationProperty
     private SocketSettings socket;
+
     /**
      * The heartbeat socket settings
      */
     @NestedConfigurationProperty
     private SocketSettings heartbeatSocket;
+
+    /**
+     * The cluster settings
+     */
+    @NestedConfigurationProperty
+    private ClusterSettings cluster;
+
+    /**
+     * Settings relating to monitoring of each server.
+     */
+    @NestedConfigurationProperty
+    private ServerSettings server;
+
+    /**
+     * All settings that relate to the pool of connections to a MongoDB server.
+     */
+    @NestedConfigurationProperty
+    private ConnectionPoolSettings connectionPool;
+
+    /**
+     * Settings for connecting to MongoDB via SSL.
+     */
+    @NestedConfigurationProperty
+    private SslSettings ssl;
 }
